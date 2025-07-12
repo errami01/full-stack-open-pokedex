2a8a876cd27c762adafda359ceed77671c971334
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2025-07-12 16:05:57 +0200

    Add version route

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const app = express()
 // get the port from env variable
 const PORT = process.env.PORT || 5000
 
+app.get('/version', (req, res) => {
+  res.send('1') // change this string to ensure a new version deployed
+})
 app.use(express.static('dist'))
-
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`)
 })
