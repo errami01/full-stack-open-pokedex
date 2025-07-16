de94f48b4329f1241784c4c4a4def9bad9d13e02
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2025-07-16 22:45:05 +0200

    Add health check route

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const PORT = process.env.PORT || 5000
 app.get('/version', (req, res) => {
   res.send('1') // change this string to ensure a new version deployed
 })
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
 app.use(express.static('dist'))
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`)
